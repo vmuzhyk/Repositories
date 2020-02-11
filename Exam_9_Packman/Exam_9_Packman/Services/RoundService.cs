0e0fa0530008a43d78bd5e6bc523819580ddce80
Vitaliy Muzhyk <vmuzhyk@outlook.com>
2020-02-11 23:29:35 +0200

    Start implement moving method of player #14

--- Exam_9_Packman/Exam_9_Packman/Services/RoundService.cs
+++ Exam_9_Packman/Exam_9_Packman/Services/RoundService.cs
@@ -10,6 +10,7 @@ namespace Exam_9_Packman.Services
     class RoundService
     {
         public Player Player { get; set; }
+        public string ChoosenDirection { get; set; }
 
         public RoundService()
         {
@@ -20,6 +21,7 @@ namespace Exam_9_Packman.Services
         {
             Player.Name = SetPlayersName();
             Console.WriteLine($"Hi {Player.Name}!");
+            DisplayMoveDialog();
         }
 
         public string SetPlayersName()
@@ -28,6 +30,53 @@ namespace Exam_9_Packman.Services
             Console.Write("Enter your name: ");
             var input = Console.ReadLine();
             return input;
+        }
+
+        public void DisplayMoveDialog()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Enter one of numbers 4, 5, 6 or 8, please ");
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "4":
+                        Console.WriteLine("You move left");
+                        break;
+                    case "5":
+                        Console.WriteLine("You move down");
+                        break;
+                    case "6":
+                        Console.WriteLine("You move right");
+                        break;
+                    case "8":
+                        Console.WriteLine("You move up");
+                        break;
+                    default:
+                        Console.WriteLine("You enter not valid number or not a number at all");
+                        break;
+                }
+                if (Player.CurrentHealth == 0)
+                    return;
+            }
+
+            /*private bool IsInputValid(bool isInteger, int number)
+            {
+                if (!isInteger)
+                {
+                    Console.WriteLine("Number should be an integer value");
+                    return false;
+                }
+
+                if (number < 0 || number > TeamB.AliveUnits.Count - 1)
+                {
+                    Console.WriteLine($"Number should be between 0 and {TeamB.AliveUnits.Count - 1 }");
+                    return false;
+                }
+
+                return true;
+            }*/
+
         }
     }
 }
